f3dd23603e8e696504d7fea549364212ef98bdfc
simjaehune <86578389+simjaehune@users.noreply.github.com>
2021-06-29 11:14:44 +0900

    Update ever.java

--- ever.java
+++ ever.java
@@ -3,68 +3,46 @@ package helloworld;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ever1 {
+public class ever2 {
+	public static int user_id;
+	public static int kid_price = 25000;
+	public static int adult_price = 50000;
+	public static int date;
+	public static int ea;
+	public static int result;
+	public static int discount;
+	public static String adl = "성인";
+	public static String kid = "아이";
+	public static String human =null;
+	public static String woodea =null;
+	public static ArrayList<String> savehuman = new ArrayList<String>();
+	public static ArrayList<String> savewoodea = new ArrayList<String>();
+	public static ArrayList<Integer> savedate = new ArrayList<Integer>();
+	public static ArrayList<Integer> saveea = new ArrayList<Integer>();
+	public static ArrayList<Integer> saveresult = new ArrayList<Integer>();
+	public static int ordercount = 0;
+	public static int type;
+	
+	
 	public static void main(String arg[]) {
-		int user_id;
-		int kid_price = 25000;
-		int adult_price = 50000;
-		int date;
-		int ea;
-		int result;
-		int discount;
-		String adl = "성인";
-		String kid = "아이";
-		String human =null;
-		String woodea =null;
-		ArrayList<String> savehuman = new ArrayList<String>();
-		ArrayList<String> savewoodea = new ArrayList<String>();
-		ArrayList<Integer> savedate = new ArrayList<Integer>();
-		ArrayList<Integer> saveea = new ArrayList<Integer>();
-		ArrayList<Integer> saveresult = new ArrayList<Integer>();
-		int ordercount = 0;
-		int type;
 		
 		do {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("이용 날짜를 입력하세요. (ex 210624)");
-		date = scan.nextInt();
-		System.out.println("이용 고객의 나이를 입력하세요.");
-		user_id = scan.nextInt();
-		System.out.println("사려는 티켓 갯수를 입력하세요");
-		ea = scan.nextInt();
-		System.out.println("우대사항을 입력하세요. \r\n"
-				+ "1. 없음 \r\n"
-				+ "2. 장애인\r\n"
-				+ "3. 국가유공자\r\n"
-				+ "4. 다자녀\r\n"
-				+ "5. 임산부");
-		discount = scan.nextInt();
-	
-		if (user_id >= 20) {
-		result = adult_price;
-		human = adl;
-		} else {
-	    result = kid_price;
-	    human = kid;
-		}
-		
-		if(discount == 1) {
-		result = (int) ((result * ea) * 1);
-		woodea = "정상가";
-		} else if(discount == 2) {
-		result = (int) ((result * ea) * 0.8);
-		woodea = "장애인 우대";
-	    } else if(discount == 3) {
-	    result = (int) ((result * ea) * 0.8);
-	    woodea = "국가유공자 우대";
-	    } else if(discount == 4) {
-		result = (int) ((result * ea) * 0.8);
-		woodea = "다자녀우대";
-		} else {
-		result = (int) ((result * ea) * 0.8);
-		woodea = "임산부 우대";
-		}
-		
+			Scanner scan = new Scanner(System.in);
+			System.out.println("이용 날짜를 입력하세요. (ex 210624)");
+			date = scan.nextInt();
+			System.out.println("이용 고객의 나이를 입력하세요.");
+			user_id = scan.nextInt();
+			System.out.println("사려는 티켓 갯수를 입력하세요");
+			ea = scan.nextInt();
+			System.out.println("우대사항을 입력하세요. \r\n"
+					+ "1. 없음 \r\n"
+					+ "2. 장애인\r\n"
+					+ "3. 국가유공자\r\n"
+					+ "4. 다자녀\r\n"
+					+ "5. 임산부");
+			discount = scan.nextInt();
+		age();
+		woo();
 		savedate.add(date);
 		savehuman.add(human);
 		saveea.add(ea);
@@ -75,14 +53,45 @@ public class ever1 {
 		type = scan.nextInt();
 		ordercount++;
 	    } while(type == 1);
-	    
+	    print();
+
+	}
+	private static void woo() {
+		// TODO Auto-generated method stub
+		if(discount == 1) {
+			result = (int) ((result * ea) * 1);
+			woodea = "정상가";
+			} else if(discount == 2) {
+			result = (int) ((result * ea) * 0.8);
+			woodea = "장애인 우대";
+		    } else if(discount == 3) {
+		    result = (int) ((result * ea) * 0.8);
+		    woodea = "국가유공자 우대";
+		    } else if(discount == 4) {
+			result = (int) ((result * ea) * 0.8);
+			woodea = "다자녀우대";
+			} else {
+			result = (int) ((result * ea) * 0.8);
+			woodea = "임산부 우대";
+			}
+		
+	}
+	private static void age() {
+		if (user_id >= 20) {
+			result = adult_price;
+			human = adl;
+			} else {
+		    result = kid_price;
+		    human = kid;
+			}
+	}
+	private static void print() {
 		System.out.println("가격은 " + result + "원 입니다");
 		System.out.println("감사합니다.");
 		System.out.println("=============== 에버랜드 =====================");
 		for(int index = 0; index < ordercount; index++) {
 		System.out.println("날짜:" + savedate.get(index) + " " + savehuman.get(index) + " X " + saveea.get(index) +"명 "+ saveresult.get(index) + "원 " + savewoodea.get(index) + " 감사합니다");
 		System.out.println("============================================");
-
 	}
-	}
+}
 }
