f602a5382edef6c232a55e824e0211d9fee3f339
simjaehune <86578389+simjaehune@users.noreply.github.com>
2021-06-30 13:41:09 +0900

    Add files via upload

---
+++ src/everarray.java
@@ -0,0 +1,12 @@
+package myever;
+
+import java.util.ArrayList;
+
+public class everarray {
+	ArrayList<String> savehuman = new ArrayList<String>();
+	ArrayList<String> savewoodea = new ArrayList<String>();
+	ArrayList<Integer> savedate = new ArrayList<Integer>();
+	ArrayList<Integer> saveea = new ArrayList<Integer>();
+	ArrayList<Integer> saveresult = new ArrayList<Integer>();
+	
+}
