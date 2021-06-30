cdac87020e30abf11614c0017a808bf2a647c689
simjaehune <86578389+simjaehune@users.noreply.github.com>
2021-06-30 13:40:48 +0900

    Add files via upload

---
+++ src/DataClass.java
@@ -0,0 +1,11 @@
+package myever;
+
+public class DataClass {
+		public String ticket = null;
+		public int count = 0;
+		public int price = 0;
+		public String age = null;
+		public String b = null;
+		public int isEventcoupon = 0;
+		
+}
