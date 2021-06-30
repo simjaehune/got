c9bedce6b765fa6d878dcb97710bdd5deadefda0
simjaehune <86578389+simjaehune@users.noreply.github.com>
2021-06-30 13:41:39 +0900

    Add files via upload

---
+++ src/everdata.java
@@ -0,0 +1,20 @@
+package myever;
+
+public class everdata {
+	public int user_id;
+	public int kid_price = 25000;
+	public int adult_price = 50000;
+	public int date;
+	public int ea;
+	public int result;
+	public int discount;
+	public String adl = "성인";
+	public String kid = "아이";
+	public String human =null;
+	public String woodea =null;
+	public int ordercount;
+	int type;
+	
+	everarray arr = new everarray();
+
+}
